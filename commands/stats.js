5a08ed57ccef1bd38b766f5f9dc9b5f86fa20134
TheEssem <smswessem@gmail.com>
2019-09-14 23:22:29 -0500

    Added stats, fixed some bugs, removed unneeded dependencies

---
+++ commands/stats.js
@@ -0,0 +1,40 @@
+const client = require("../utils/client.js");
+const moment = require("moment");
+require("moment-duration-format");
+const os = require("os");
+
+exports.run = async (message) => {
+  const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+  const embed = {
+    embed: {
+      "author": {
+        "name": "esmBot Statistics",
+        "icon_url": client.user.avatarURL
+      },
+      "color": 16711680,
+      "description": "**You are currently using esmBot Dev! Things may change at any time without warning and there will be bugs. Many bugs.**",
+      "fields": [{
+        "name": "Memory Usage",
+        "value": `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`
+      },
+      {
+        "name": "Uptime",
+        "value": duration
+      },
+      {
+        "name": "Host",
+        "value": `${os.type()} ${os.release()} (${os.arch()})`
+      },
+      {
+        "name": "Library",
+        "value": `Eris ${require("eris").VERSION}`
+      },
+      {
+        "name": "Node.js Version",
+        "value": process.version
+      }
+      ]
+    }
+  };
+  return message.channel.createMessage(embed);
+};
--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,7 +5,7 @@ const client = require("../utils/client.js");
 
 // run when the bot is added to a guild
 module.exports = async (guild) => {
-  logger.log("info", `[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${client.users.get(guild.ownerID).username + client.users.get(guild.ownerID).discriminator} (${guild.ownerID})`);
+  logger.log("info", `[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${client.users.get(guild.ownerID).username}#${client.users.get(guild.ownerID).discriminator} (${guild.ownerID})`);
   database.settings.set(guild.id, misc.defaults);
   database.tags.set(guild.id, misc.tagDefaults);
 };
