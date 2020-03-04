c28c575b482f0a9f27ceb72613199759a19a160f
Essem <smswessem@gmail.com>
2020-03-04 10:38:46 -0600

    Fixed sizing for caption

--- commands/caption.js
+++ commands/caption.js
@@ -10,7 +10,7 @@ exports.run = async (message, args) => {
   const processMessage = await message.channel.createMessage("<a:processing:479351417102925854> Processing... This might take a while");
   const output = await gm(image.path).out("-size", "%[fx:w]x").background("white").fill("black").font("./assets/caption.otf", "%[fx:w/10]").out("-delete", "0--1").gravity("Center").out(`caption:${args.join(" ")}`).bufferPromise("png");
   const output2 = await gm(output).out(image.path).gravity("Center").trim().out("+repage").extent("%[fx:v.w]", "%[fx:u.h + ( v.w/10 )]").out("-delete", "1--1").streamPromise();
-  const outputFinal = await gm(output2).out("-alpha", "set").background("none").out("(").out(image.path).out("-coalesce").out(")").colorspace("sRGB").out("-set", "page", "%[fx:u.w]x%[fx:u.h+v.h]+%[fx:t?(u.w-v.w)/2:0]+%[fx:t?u.h:0]").out("-coalesce").out("null:").out("-insert", 1).out("-layers", "composite").bufferPromise(image.type);
+  const outputFinal = await gm(output2).out("-alpha", "set").background("none").out("(").out(image.path).out("-coalesce").out(")").colorspace("sRGB").out("-set", "page", "%[fx:v.w]x%[fx:u.h+v.h]+%[fx:t?(u.w-v.w)/2:0]+%[fx:t?u.h:0]").out("-coalesce").out("null:").out("-insert", 1).out("-layers", "composite").bufferPromise(image.type);
   await processMessage.delete();
   //return upload(message, outputFinal, `caption.${image.type}`);
   return message.channel.createMessage("", {
