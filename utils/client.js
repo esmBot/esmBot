58f305dbbc834465921626802af225369e8f1ba8
TheEssem <smswessem@gmail.com>
2020-03-12 08:25:20 -0500

    Added some extra settings to speed up the client

--- utils/client.js
+++ utils/client.js
@@ -1,6 +1,23 @@
 // separate the client from app.js so we can call it later
 const { Client } = require("eris");
 const client = new Client(process.env.TOKEN, {
-  defaultImageSize: 1024
+  defaultImageSize: 1024,
+  disableEvents: {
+    CHANNEL_CREATE: true,
+    CHANNEL_DELETE: true,
+    CHANNEL_UPDATE: true,
+    GUILD_BAN_REMOVE: true,
+    GUILD_MEMBER_ADD: true,
+    GUILD_MEMBER_REMOVE: true,
+    GUILD_MEMBER_UPDATE: true,
+    GUILD_ROLE_CREATE: true,
+    GUILD_ROLE_DELETE: true,
+    GUILD_ROLE_UPDATE: true,
+    TYPING_START: true,
+    USER_UPDATE: true,
+    VOICE_STATE_UPDATE: true
+  },
+  maxShards: "auto",
+  opusOnly: true
 });
 module.exports = client;
