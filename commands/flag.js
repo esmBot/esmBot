6056e49728896629bd7307d24200e438dddd3cdb
TheEssem <smswessem@gmail.com>
2020-06-21 12:12:58 -0500

    Changed source of flags, added new activity message

--- commands/flag.js
+++ commands/flag.js
@@ -10,11 +10,10 @@ exports.run = async (message, args) => {
   if (image === undefined) return `${message.author.mention}, you need to provide an image to overlay a flag onto!`;
   if (!args[0].match(emojiRegex)) return `${message.author.mention}, you need to provide an emoji of a flag to overlay!`;
   const flag = emoji.unemojify(args[0]).replace(/:/g, "").replace("flag-", "");
-  let path = `./assets/images/country-flags/png1000px/${flag}.png`;
+  let path = `./assets/images/region-flags/png/${flag.toUpperCase()}.png`;
   if (flag === "🏴‍☠️") path = "./assets/images/pirateflag.png";
-  console.log(flag);
   const size = await gm(image.path).sizePromise();
-  const buffer = await gm(image.path).coalesce().out("null:").out("(", path, "-alpha", "set", "-channel", "A", "-evaluate", "multiply", "0.4", "+channel", ")").gravity("North").scale(...(size.width < size.height ? ["%[fx:u.w]", null] : [null, "%[fx:u.h]"])).out("-layers", "composite").bufferPromise(image.type, image.delay);
+  const buffer = await gm(image.path).coalesce().out("null:").out("(", path, "-alpha", "set", "-channel", "A", "-evaluate", "multiply", "0.5", "+channel", ")").gravity("North").scale(...(size.width < size.height ? ["%[fx:u.w]", null] : [null, "%[fx:u.h]"])).out("-layers", "composite").bufferPromise(image.type, image.delay);
   return {
     file: buffer,
     name: `flag.${image.type}`
