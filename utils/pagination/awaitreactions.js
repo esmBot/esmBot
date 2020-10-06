f1404a83935a1e0a9d7b81671d1ac178f6e74d90
TheEssem <smswessem@gmail.com>
2020-10-06 10:18:33 -0500

    Fixed compatibility with Eris 0.13, get patron list dynamically

--- utils/pagination/awaitreactions.js
+++ utils/pagination/awaitreactions.js
@@ -11,16 +11,16 @@ class ReactionCollector extends EventEmitter {
     this.ended = false;
     this.collected = [];
     this.bot = client;
-    this.listener = (message, emoji, userID) => this.verify(message, emoji, userID);
+    this.listener = async (message, emoji, userID) => await this.verify(message, emoji, userID);
     this.bot.on("messageReactionAdd", this.listener);
     if (options.time) setTimeout(() => this.stop("time"), options.time);
   }
 
-  verify(message, emoji, userID) {
+  async verify(message, emoji, userID) {
     if (this.message.id !== message.id) return false;
     if (this.filter(message, emoji, userID)) {
       this.collected.push({ message: message, emoji: emoji, userID: userID });
-      this.emit("reaction", message, emoji, userID);
+      this.emit("reaction", await client.getMessage(message.channel.id, message.id), emoji, userID);
       if (this.collected.length >= this.options.maxMatches) this.stop("maxMatches");
       return true;
     }
