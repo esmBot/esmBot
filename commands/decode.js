ae1406b518e4728e28866612ef5d83bdec8ea056
TheEssem <smswessem@gmail.com>
2020-12-29 22:26:45 -0600

    *final* final changes

--- commands/decode.js
+++ commands/decode.js
@@ -1,7 +1,9 @@
+const { clean } = require("../utils/misc.js");
+
 exports.run = async (message, args) => {
   if (args.length === 0) return `${message.author.mention}, you need to provide a string to decode!`;
   const b64Decoded = Buffer.from(args.join(" "), "base64").toString("utf-8");
-  return `\`\`\`\n${b64Decoded}\`\`\``;
+  return `\`\`\`\n${await clean(b64Decoded)}\`\`\``;
 };
 
 exports.aliases = ["b64decode", "base64decode"];
