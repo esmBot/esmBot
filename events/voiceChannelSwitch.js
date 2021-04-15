3f1b36670b2e4341233d29e9d83de41f741cb9bd
TheEssem <smswessem@gmail.com>
2021-04-15 13:59:57 -0500

    Fixed voice channel switching issue

--- events/voiceChannelSwitch.js
+++ events/voiceChannelSwitch.js
@@ -1,5 +1,5 @@
 const leaveHandler = require("./voiceChannelLeave.js");
 
 module.exports = async (client, member, newChannel, oldChannel) => {
-  await leaveHandler(member, oldChannel);
+  await leaveHandler(client, member, oldChannel);
 };
