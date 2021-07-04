93d2b0e5ceb792d79e36c873b611e4393ed86860
Essem <smswessem@gmail.com>
2021-07-03 22:05:00 -0500

    Renamed gif to raw and fixed message, removed GOOGLE variable from .env.example

---
+++ commands/general/raw.js
@@ -0,0 +1,16 @@
+const Command = require("../../classes/command.js");
+const imageDetect = require("../../utils/imagedetect.js");
+
+class RawCommand extends Command {
+  async run() {
+    this.message.channel.sendTyping();
+    const image = await imageDetect(this.client, this.message);
+    if (image === undefined) return "You need to provide an image to get a raw URL!";
+    return image.path;
+  }
+
+  static description = "Gets a direct image URL (useful for saving GIFs from sites like Tenor)";
+  static aliases = ["gif", "getgif", "giflink", "imglink", "getimg", "rawgif", "rawimg"];
+}
+
+module.exports = RawCommand;
