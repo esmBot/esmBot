75258f3ebc39e70941fdfb77554b351aa43e5962
Essem <smswessem@gmail.com>
2021-08-09 08:33:16 -0500

    Remove the cache limit entirely

--- utils/collections.js
+++ utils/collections.js
@@ -14,7 +14,7 @@ class TimedMap extends Map {
 
 exports.runningCommands = new TimedMap();
 
-class Cache extends Map {
+/*class Cache extends Map {
   constructor(values) {
     super(values);
     this.maxValues = 2048;
@@ -24,7 +24,7 @@ class Cache extends Map {
     super.set(key, value);
     if (this.size > this.maxValues) this.delete(this.keys().next().value);
   }
-}
+}*/
 
-exports.prefixCache = new Cache();
-exports.disabledCache = new Cache();
+exports.prefixCache = new Map();
+exports.disabledCache = new Map();
