1493d8b4c281d36907cb7cf28dfaba36786ae70b
ItsODRJR <69770237+ItsODRJR@users.noreply.github.com>
2021-12-02 15:07:07 -0600

    Add files via upload

---
+++ natives/zamn.cc
@@ -0,0 +1,62 @@
+#include <Magick++.h>
+#include <napi.h>
+
+#include <list>
+
+using namespace std;
+using namespace Magick;
+
+Napi::Value Zamn(const Napi::CallbackInfo &info) {
+  Napi::Env env = info.Env();
+
+  try {
+    Napi::Object obj = info[0].As<Napi::Object>();
+    Napi::Buffer<char> data = obj.Get("data").As<Napi::Buffer<char>>();
+    string type = obj.Get("type").As<Napi::String>().Utf8Value();
+    int delay =
+        obj.Has("delay") ? obj.Get("delay").As<Napi::Number>().Int32Value() : 0;
+
+    Blob blob;
+
+    list<Image> frames;
+    list<Image> coalesced;
+    list<Image> mid;
+    Image watermark;
+    readImages(&frames, Blob(data.Data(), data.Length()));
+    watermark.read("./assets/images/zamn.png");
+    coalesceImages(&coalesced, frames.begin(), frames.end());
+
+    for (Image &image : coalesced) {
+      Image watermark_new = watermark;
+      image.backgroundColor("none");
+      image.scale(Geometry("303x438!"));
+      image.extent(Geometry("621x516-310-75"));
+      watermark_new.composite(image, Magick::CenterGravity,
+                              Magick::OverCompositeOp);
+      watermark_new.magick(type);
+      watermark_new.animationDelay(delay == 0 ? image.animationDelay() : delay);
+      mid.push_back(watermark_new);
+    }
+
+    optimizeTransparency(mid.begin(), mid.end());
+
+    if (type == "gif") {
+      for (Image &image : mid) {
+        image.quantizeDitherMethod(FloydSteinbergDitherMethod);
+        image.quantize();
+      }
+    }
+
+    writeImages(mid.begin(), mid.end(), &blob);
+
+    Napi::Object result = Napi::Object::New(env);
+    result.Set("data", Napi::Buffer<char>::Copy(env, (char *)blob.data(),
+                                                blob.length()));
+    result.Set("type", type);
+    return result;
+  } catch (std::exception const &err) {
+    throw Napi::Error::New(env, err.what());
+  } catch (...) {
+    throw Napi::Error::New(env, "Unknown error");
+  }
+}
