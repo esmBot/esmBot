e66f45ba0f26a3a3531e300837acdd1b177932af
Essem <smswessem@gmail.com>
2021-12-26 23:06:05 -0600

    Fixed clean always resulting in <redacted>

--- utils/misc.js
+++ utils/misc.js
@@ -27,8 +27,8 @@ export async function clean(text) {
   const imageServers = JSON.parse(fs.readFileSync("./servers.json", { encoding: "utf8" })).image;
 
   for (const { server, auth } of imageServers) {
-    text = optionalReplace(server);
-    text = optionalReplace(auth);
+    text = text.replaceAll(server, optionalReplace(server));
+    text = text.replaceAll(auth, optionalReplace(auth));
   }
 
   for (const env of Object.keys(parsed)) {
