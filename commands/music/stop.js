fb0c8ffae17557b5249b4039b81ccf43a58050e6
Essem <smswessem@gmail.com>
2022-01-05 18:55:42 -0600

    Handle specific edge case in stop

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -6,6 +6,10 @@ class StopCommand extends MusicCommand {
     if (!this.message.channel.guild) return "This command only works in servers!";
     if (!this.message.member.voiceState.channelID) return "You need to be in a voice channel first!";
     if (!this.message.channel.guild.members.get(this.client.user.id).voiceState.channelID) return "I'm not in a voice channel!";
+    if (!this.connection) {
+      await manager.leave(this.message.channel.guild.id);
+      return "🔊 The current voice channel session has ended.";
+    }
     if (this.connection.host !== this.message.author.id && !this.message.member.permissions.has("manageChannels")) return "Only the current voice session host can stop the music!";
     await manager.leave(this.message.channel.guild.id);
     const connection = this.connection.player;
