bbfbbd6e08047ed60b51daf0c85de220fe4f659a
Seif <seifaziz10@gmail.com>
2022-01-23 20:40:11 -0800

    Add damn daniel command and ogg file

---
+++ commands/soundboard/damndaniel.js
@@ -0,0 +1,13 @@
+import { play } from "../../utils/soundplayer.js";
+import MusicCommand from "../../classes/musicCommand.js";
+
+class DamnDanielCommand extends MusicCommand {
+    async run() {
+        return await play(this.client, "./assets/audio/damndaniel.ogg", this.message);
+    }
+
+    static description = "Plays the \"damn daniel\" sound effect";
+    static aliases = ["daniel"];
+}
+
+export default DamnDanielCommand;
