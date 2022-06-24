f6f90914673cab56f89e1de460a26495a51a75ea
Angel <59970528+angeld23@users.noreply.github.com>
2022-06-24 18:22:07 -0400

    Make help message for &stretch accurate to the aspect ratio it applies (#284)

--- commands/image-editing/stretch.js
+++ commands/image-editing/stretch.js
@@ -5,8 +5,8 @@ class StretchCommand extends ImageCommand {
     stretch: true
   };
 
-  static description = "Stretches an image to a 4:3 aspect ratio";
-  static aliases = ["aspect", "ratio", "aspect43", "43"];
+  static description = "Stretches an image to a 1:1 aspect ratio";
+  static aliases = ["aspect", "ratio", "aspect11", "11"];
 
   static noImage = "You need to provide an image/GIF to stretch!";
   static command = "resize";
