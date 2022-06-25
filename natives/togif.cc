9de9ce399ce3fd1f4ab71a0c4aa4ed435c4a1f50
bjcscat <bjcs.rbx@gmail.com>
2022-06-25 06:07:53 +0000

    use libvips not magick

--- natives/togif.cc
+++ natives/togif.cc
@@ -1,11 +1,9 @@
-#include <Magick++.h>
 #include <napi.h>
 
-#include <iostream>
-#include <list>
+#include <vips/vips8>
 
 using namespace std;
-using namespace Magick;
+using namespace vips;
 
 Napi::Value ToGif(const Napi::CallbackInfo &info) {
   Napi::Env env = info.Env();
@@ -14,37 +12,40 @@ Napi::Value ToGif(const Napi::CallbackInfo &info) {
     Napi::Object obj = info[0].As<Napi::Object>();
     Napi::Buffer<char> data = obj.Get("data").As<Napi::Buffer<char>>();
     string type = obj.Get("type").As<Napi::String>().Utf8Value();
-    int delay =
-        obj.Has("delay") ? obj.Get("delay").As<Napi::Number>().Int32Value() : 0;
 
-    Blob blob;
-
-    list<Image> frames;
-    list<Image> coalesced;
-    try {
-      readImages(&frames, Blob(data.Data(), data.Length()));
-    } catch (Magick::WarningCoder &warning) {
-      cerr << "Coder Warning: " << warning.what() << endl;
-    } catch (Magick::Warning &warning) {
-      cerr << "Warning: " << warning.what() << endl;
-    }
-    coalesceImages(&coalesced, frames.begin(), frames.end());
-
-    for_each(coalesced.begin(), coalesced.end(), magickImage(type));
-
-    optimizeTransparency(coalesced.begin(), coalesced.end());
-
-    for (Image &image : coalesced) {
-        image.quantizeDitherMethod(FloydSteinbergDitherMethod);
-        image.quantize();
-        if (delay != 0) image.animationDelay(delay);
+    if (type == "gif") {
+      Napi::Object result = Napi::Object::New(env);
+      result.Set("data", data);
+      result.Set("type", "gif");
+      return result;
     }
 
-    writeImages(coalesced.begin(), coalesced.end(), &blob);
+    VOption *options = VImage::option()->set("access", "sequential");
+
+    VImage in =
+        VImage::new_from_buffer(data.Data(), data.Length(), "",
+                                type == "gif" ? options->set("n", -1) : options)
+            .colourspace(VIPS_INTERPRETATION_sRGB);
+    if (!in.has_alpha()) in = in.bandjoin(255);
+
+    int pageHeight = vips_image_get_page_height(in.get_image());
+
+    vector<VImage> img;
+
+    img.push_back(in);
+
+    VImage final = VImage::arrayjoin(img, VImage::option()->set("across", 1));
+    final.set(VIPS_META_PAGE_HEIGHT, pageHeight + in.height());
+
+    void *buf;
+    size_t length;
+    final.write_to_buffer(
+        ("." + type).c_str(), &buf, &length, VImage::option()->set("dither", 0));
+
+    vips_thread_shutdown();
 
     Napi::Object result = Napi::Object::New(env);
-    result.Set("data", Napi::Buffer<char>::Copy(env, (char *)blob.data(),
-                                                blob.length()));
+    result.Set("data", Napi::Buffer<char>::Copy(env, (char *)buf, length));
     result.Set("type", "gif");
     return result;
   } catch (std::exception const &err) {
