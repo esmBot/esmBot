fe97d38178cc1f7713c95297d17271e7f88929f4
jonah <73760377+jonerrr@users.noreply.github.com>
2022-10-11 13:14:26 -0400

    fix formatting

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ Please refer to step 3 of the setup guide.`);
   process.exit(1);
 }
 if (process.platform === "win32") {
-  console.error(
-    "\x1b[1m\x1b[31m\x1b[40m" +
-      `WINDOWS IS NOT OFFICIALLY SUPPORTED!
+  console.error("\x1b[1m\x1b[31m\x1b[40m" + `WINDOWS IS NOT OFFICIALLY SUPPORTED!
 Although there's a (very) slim chance of it working, multiple aspects of the bot are built with UNIX-like systems in mind and could break on Win32-based systems. If you want to run the bot on Windows, using Windows Subsystem for Linux is highly recommended.
-The bot will continue to run past this message in 5 seconds, but keep in mind that it could break at any time. Continue running at your own risk; alternatively, stop the bot using Ctrl+C and install WSL.` +
-      "\x1b[0m"
-  );
+The bot will continue to run past this message in 5 seconds, but keep in mind that it could break at any time. Continue running at your own risk; alternatively, stop the bot using Ctrl+C and install WSL.` + "\x1b[0m");
   Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 5000);
 }
 
@@ -44,14 +40,14 @@ import { reload, connect, connected } from "./utils/soundplayer.js";
 import { endBroadcast, startBroadcast } from "./utils/misc.js";
 import { parseThreshold } from "./utils/tempimages.js";
 
-const { types } = JSON.parse(
-  readFileSync(new URL("./config/commands.json", import.meta.url))
-);
-const esmBotVersion = JSON.parse(
-  readFileSync(new URL("./package.json", import.meta.url))
-).version;
+const { types } = JSON.parse(readFileSync(new URL("./config/commands.json", import.meta.url)));
+const esmBotVersion = JSON.parse(readFileSync(new URL("./package.json", import.meta.url))).version;
 
-const intents = ["GUILD_VOICE_STATES", "DIRECT_MESSAGES", "GUILDS"];
+const intents = [
+  "GUILD_VOICE_STATES",
+  "DIRECT_MESSAGES",
+  "GUILDS"
+];
 if (types.classic) {
   intents.push("GUILD_MESSAGES");
   intents.push("MESSAGE_CONTENT");
@@ -60,8 +56,7 @@ if (types.classic) {
 async function* getFiles(dir) {
   const dirents = await promises.readdir(dir, { withFileTypes: true });
   for (const dirent of dirents) {
-    const name =
-      dir + (dir.charAt(dir.length - 1) !== "/" ? "/" : "") + dirent.name;
+    const name = dir + (dir.charAt(dir.length - 1) !== "/" ? "/" : "") + dirent.name;
     if (dirent.isDirectory()) {
       yield* getFiles(name);
     } else if (dirent.name.endsWith(".js")) {
@@ -71,12 +66,7 @@ async function* getFiles(dir) {
 }
 
 async function init() {
-  await exec("git rev-parse HEAD")
-    .then(
-      (output) => output.stdout.substring(0, 7),
-      () => "unknown commit"
-    )
-    .then((o) => (process.env.GIT_REV = o));
+  await exec("git rev-parse HEAD").then(output => output.stdout.substring(0, 7), () => "unknown commit").then(o => process.env.GIT_REV = o);
   console.log(`
      ,*\`$                    z\`"v       
     F zBw\`%                 A ,W "W     
@@ -102,9 +92,7 @@ esmBot ${esmBotVersion} (${process.env.GIT_REV})
 `);
 
   if (!types.classic && !types.application) {
-    logger.error(
-      "Both classic and application commands are disabled! Please enable at least one command type in config/commands.json."
-    );
+    logger.error("Both classic and application commands are disabled! Please enable at least one command type in config/commands.json.");
     return process.exit(1);
   }
 
@@ -119,9 +107,7 @@ esmBot ${esmBotVersion} (${process.env.GIT_REV})
 
   // register commands and their info
   logger.log("info", "Attempting to load commands...");
-  for await (const commandFile of getFiles(
-    resolve(dirname(fileURLToPath(import.meta.url)), "./commands/")
-  )) {
+  for await (const commandFile of getFiles(resolve(dirname(fileURLToPath(import.meta.url)), "./commands/"))) {
     logger.log("main", `Loading command from ${commandFile}...`);
     try {
       await load(null, commandFile);
@@ -141,43 +127,34 @@ esmBot ${esmBotVersion} (${process.env.GIT_REV})
       everyone: false,
       roles: false,
       users: true,
-      repliedUser: true,
+      repliedUser: true
     },
     gateway: {
       concurrency: "auto",
       maxShards: "auto",
-      shardIDs: process.env.SHARDS
-        ? JSON.parse(process.env.SHARDS)[process.env.pm_id - 1]
-        : null,
+      shardIDs: process.env.SHARDS ? JSON.parse(process.env.SHARDS)[process.env.pm_id - 1] : null,
       presence: {
         status: "idle",
-        activities: [
-          {
-            type: 0,
-            name: "Starting esmBot...",
-          },
-        ],
+        activities: [{
+          type: 0,
+          name: "Starting esmBot..."
+        }]
       },
-      intents,
+      intents
     },
     collectionLimits: {
-      messages: 50,
-    },
+      messages: 50
+    }
   });
 
   // register events
   logger.log("info", "Attempting to load events...");
-  for await (const file of getFiles(
-    resolve(dirname(fileURLToPath(import.meta.url)), "./events/")
-  )) {
+  for await (const file of getFiles(resolve(dirname(fileURLToPath(import.meta.url)), "./events/"))) {
     logger.log("main", `Loading event from ${file}...`);
     const eventArray = file.split("/");
     const eventName = eventArray[eventArray.length - 1].split(".")[0];
     if (eventName === "interactionCreate" && !types.application) {
-      logger.log(
-        "warn",
-        `Skipped loading event from ${file} because application commands are disabled`
-      );
+      logger.log("warn", `Skipped loading event from ${file} because application commands are disabled`);
       continue;
     }
     const { default: event } = await import(file);
@@ -212,22 +189,18 @@ esmBot ${esmBotVersion} (${process.env.GIT_REV})
             endBroadcast(client);
             break;
           case "serverCounts":
-            pm2.sendDataToProcessId(
-              0,
-              {
-                id: 0,
-                type: "process:msg",
-                data: {
-                  type: "serverCounts",
-                  guilds: client.guilds.size,
-                  shards: client.shards.size,
-                },
-                topic: true,
+            pm2.sendDataToProcessId(0, {
+              id: 0,
+              type: "process:msg",
+              data: {
+                type: "serverCounts",
+                guilds: client.guilds.size,
+                shards: client.shards.size
               },
-              (err) => {
-                if (err) logger.error(err);
-              }
-            );
+              topic: true
+            }, (err) => {
+              if (err) logger.error(err);
+            });
             break;
         }
       });
