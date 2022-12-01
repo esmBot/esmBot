esmBot/esmBot

commit 16095c02567fce78d203bcba327fe5c790644365

Prefix shouldn't be null ever, let alone when message.channel.guild isn't defined. (#276)
* '/eval 9+10' returns 21

* undid 9+10 joke, fixed prefix bug

* nope that's not how it was

* fully undid 9+10 joke oops

events/messageCreate.js
@@ -43,7 +43,7 @@ export default async (client, cluster, worker, ipc, message) => {
       prefix = prefixCandidate;
     }
   } else {
-    prefix = "";
+    prefix = process.env.PREFIX;
   }
 
   // ignore other stuff

commands/general/prefix.js
@@ -3,7 +3,7 @@ import Command from "../../classes/command.js";
 
 class PrefixCommand extends Command {
   async run() {
-    if (!this.channel.guild) return "This command only works in servers!";
+    if (!this.channel.guild) return `The current prefix is \`${process.env.PREFIX}\``;
     const guild = await database.getGuild(this.channel.guild.id);
     if (this.args.length !== 0) {
       const owners = process.env.OWNER.split(",");

commit 0b17af5fd43bcda3da3a0c887231f7b02638fab1

allow running commands in DMs without prefix
this functionality was broken in
16095c02567fce78d203bcba327fe5c790644365 but is now fixed

events/messageCreate.js
@@ -47,7 +47,7 @@ export default async (client, message) => {
   } else if (message.content.startsWith(process.env.PREFIX)) {
     text = message.content.substring(process.env.PREFIX.length).trim();
   } else {
-    return;
+    text = message.content;
   }
 
   // separate commands and args