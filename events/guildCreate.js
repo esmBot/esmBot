d36ff7432606aba42002b9b847a385f827f36e54
Essem <smswessem@gmail.com>
2023-03-09 10:10:52 -0600

    why did it take me 3 goddamn months to realize this was missing

---
+++ events/guildCreate.js
@@ -0,0 +1,6 @@
+import { log } from "../utils/logger.js";
+
+// run when the bot is added to a guild
+export default async (client, guild) => {
+  log(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot.`);
+};
