a77dc5acf964fc61970c2087cbbd27c4997171fc
Essem <smswessem@gmail.com>
2023-03-10 13:05:21 -0600

    Port scott, update packages

--- natives/scott.cc
+++ natives/scott.cc
@@ -1,64 +1,55 @@
-#include <Magick++.h>
-
-#include <cstring>
-#include <iostream>
-#include <list>
+#include <vips/vips8>
 
 #include "common.h"
 
 using namespace std;
-using namespace Magick;
+using namespace vips;
 
 char *Scott(string type, string *outType, char *BufferData, size_t BufferLength,
             ArgumentMap Arguments, size_t *DataSize) {
   string basePath = GetArgument<string>(Arguments, "basePath");
 
-  Blob blob;
+  VOption *options = VImage::option()->set("access", "sequential");
 
-  list<Image> frames;
-  list<Image> coalesced;
-  list<Image> mid;
-  Image watermark;
-  try {
-    readImages(&frames, Blob(BufferData, BufferLength));
-  } catch (Magick::WarningCoder &warning) {
-    cerr << "Coder Warning: " << warning.what() << endl;
-  } catch (Magick::Warning &warning) {
-    cerr << "Warning: " << warning.what() << endl;
+  VImage in =
+      VImage::new_from_buffer(BufferData, BufferLength, "",
+                              type == "gif" ? options->set("n", -1) : options)
+          .colourspace(VIPS_INTERPRETATION_sRGB);
+  if (!in.has_alpha()) in = in.bandjoin(255);
+
+  int width = in.width();
+  int pageHeight = vips_image_get_page_height(in.get_image());
+  int nPages = vips_image_get_n_pages(in.get_image());
+
+  string assetPath = basePath + "assets/images/scott.png";
+  VImage bg = VImage::new_from_file(assetPath.c_str());
+
+  string distortPath = basePath + "assets/images/scottmap.png";
+  VImage distort = VImage::new_from_file(distortPath.c_str());
+
+  VImage distortImage =
+      ((distort[1] / 255) * 414).bandjoin((distort[0] / 255) * 233);
+
+  vector<VImage> img;
+  for (int i = 0; i < nPages; i++) {
+    VImage img_frame =
+        type == "gif" ? in.crop(0, i * pageHeight, width, pageHeight) : in;
+    VImage resized = img_frame.resize(
+        415 / (double)width,
+        VImage::option()->set("vscale", 234 / (double)pageHeight));
+    VImage mapped = resized.mapim(distortImage)
+                        .extract_band(0, VImage::option()->set("n", 3))
+                        .bandjoin(distort[2]);
+    VImage offset = mapped.embed(127, 181, 864, 481);
+    VImage composited = bg.composite2(offset, VIPS_BLEND_MODE_OVER);
+    img.push_back(composited);
   }
-  watermark.read(basePath + "assets/images/scott.png");
-  coalesceImages(&coalesced, frames.begin(), frames.end());
+  VImage final = VImage::arrayjoin(img, VImage::option()->set("across", 1));
+  final.set(VIPS_META_PAGE_HEIGHT, 481);
 
-  for (Image &image : coalesced) {
-    Image watermark_new = watermark;
-    image.virtualPixelMethod(Magick::TransparentVirtualPixelMethod);
-    image.backgroundColor("none");
-    image.scale(Geometry("415x234!"));
-    double arguments[16] = {0,   0,   129, 187, 415, 0,   517, 182,
-                            415, 234, 517, 465, 0,   234, 132, 418};
-    image.distort(Magick::PerspectiveDistortion, 16, arguments, true);
-    image.extent(Geometry("864x481"), Magick::CenterGravity);
-    watermark_new.composite(image, Geometry("-110+83"),
-                            Magick::OverCompositeOp);
-    watermark_new.magick(*outType);
-    watermark_new.animationDelay(image.animationDelay());
-    mid.push_back(watermark_new);
-  }
-
-  optimizeTransparency(mid.begin(), mid.end());
-
-  if (*outType == "gif") {
-    for (Image &image : mid) {
-      image.quantizeDitherMethod(FloydSteinbergDitherMethod);
-      image.quantize();
-    }
-  }
-
-  writeImages(mid.begin(), mid.end(), &blob);
-
-  *DataSize = blob.length();
-
-  char *data = (char *)malloc(*DataSize);
-  memcpy(data, blob.data(), *DataSize);
-  return data;
+  void *buf;
+  final.write_to_buffer(
+      ("." + *outType).c_str(), &buf, DataSize,
+      *outType == "gif" ? VImage::option()->set("dither", 1) : 0);
+  return (char *)buf;
 }
