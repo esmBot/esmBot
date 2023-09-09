258003b043a3cc3a6c9e27331565dff5d99fac8f
Essem <smswessem@gmail.com>
2023-09-09 15:01:15 -0500

    fix: :adhesive_bandage: Strip non-emoji text from flag input

--- commands/image-editing/flag.js
+++ commands/image-editing/flag.js
@@ -8,8 +8,9 @@ class FlagCommand extends ImageCommand {
 
   async criteria() {
     const text = this.options.text ?? this.args[0];
-    if (!text.match(emojiRegex())) return false;
-    const flag = emoji.unemojify(text).replaceAll(":", "").replace("flag-", "");
+    const matched = text.match(emojiRegex());
+    if (!matched) return false;
+    const flag = emoji.unemojify(matched[0]).replaceAll(":", "").replace("flag-", "");
     let path = `assets/images/region-flags/png/${flag.toUpperCase()}.png`;
     if (flag === "pirate_flag") path = "assets/images/pirateflag.png";
     if (flag === "rainbow-flag") path = "assets/images/rainbowflag.png";
