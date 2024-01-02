15a7b6a7c43491d7a5a158cd3fc3b8aea4e445a7
Essem <smswessem@gmail.com>
2024-01-01 20:41:53 -0600

    feat: :construction: Allow building natives as generic C library

---
+++ natives/generic/image.cc
@@ -0,0 +1,102 @@
+#include "../common.h"
+
+#if defined(WIN32) && defined(MAGICK_ENABLED)
+#include <Magick++.h>
+#endif
+#include <nlohmann/json.hpp>
+#include <vips/vips8>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void imageInit() {
+#if defined(WIN32) && defined(MAGICK_ENABLED)
+  Magick::InitializeMagick("");
+#endif
+  if (vips_init("")) vips_error_exit(NULL);
+  return;
+}
+
+struct image_result {
+  const char *type;
+  size_t length;
+  void *buf;
+};
+
+image_result *image(char *command, char *args, char *data, size_t length) {
+  nlohmann::json parsedArgs = nlohmann::json::parse(args);
+  ArgumentMap Arguments;
+
+  for (auto& pair : parsedArgs.items()) {
+    auto key = pair.key();
+    if (key == "data") {
+      continue;
+    }
+
+    auto val = pair.value();
+    if (val.is_boolean()) {
+      Arguments[key] = val.template get<bool>();
+    } else if (val.is_string()) {
+      Arguments[key] = val.template get<string>();
+    } else if (val.is_number_integer()) {
+      Arguments[key] = val.template get<int>();
+    } else if (val.is_number_float()) {
+      Arguments[key] = val.template get<float>();
+    } else {
+      throw "Unimplemented value type passed to image native.";
+    }
+  }
+
+  string type = GetArgumentWithFallback<string>(Arguments, "type", "png");
+  string outType = GetArgumentWithFallback<bool>(Arguments, "togif", false) ? "gif" : type;
+
+  size_t outLength = 0;
+  ArgumentMap outMap;
+  if (length == 0) {
+    outMap = FunctionMap.at(command)(type, &outType, data, length, Arguments,
+                                     &outLength);
+  } else {
+    outMap =
+        NoInputFunctionMap.at(command)(type, &outType, Arguments, &outLength);
+  }
+
+  vips_error_clear();
+  vips_thread_shutdown();
+
+  char *buf = GetArgument<char *>(outMap, "buf");
+
+  image_result *out = (image_result *)malloc(sizeof(image_result));
+  out->buf = buf;
+  out->type = type.c_str();
+  out->length = outLength;
+  return out;
+}
+
+void img_free(void *ptr, [[maybe_unused]] void *ctx) {
+  g_free(ptr);
+}
+
+char *get_funcs() {
+  nlohmann::json funcs = nlohmann::json::array();
+  for (auto const& imap : FunctionMap) {
+    funcs.push_back(imap.first);
+  }
+  for (auto const& imap : NoInputFunctionMap) {
+    funcs.push_back(imap.first);
+  }
+  string dumped = funcs.dump();
+  size_t length = dumped.length();
+  char* out = new char[length + 1];
+  out[length] = 0;
+  dumped.copy(out, length);
+  return out;
+}
+
+const char *get_type(image_result *result) { return result->type; }
+void *get_data(image_result *result) { return result->buf; }
+size_t get_size(image_result *result) { return result->length; }
+
+#ifdef __cplusplus
+}
+#endif
