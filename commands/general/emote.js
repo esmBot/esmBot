f38d022351a9006d86ff6dff60462002e10a4047
Essem <smswessem@gmail.com>
2024-01-26 20:13:00 -0600

    feat: Multiple emote improvements
    This allows for replying to a message containing emojis to get them, improves the regex used for getting custom emojis, and returns every emoji found in a message.

--- commands/general/emote.js
+++ commands/general/emote.js
@@ -3,19 +3,26 @@ import Command from "../../classes/command.js";
 
 class EmoteCommand extends Command {
   async run() {
-    const emoji = this.options.emoji ?? this.content;
-    if (emoji?.trim() && emoji.split(" ")[0].match(/^<a?:.+:\d+>$/)) {
-      return `https://cdn.discordapp.com/emojis/${emoji.split(" ")[0].replace(/^<(a)?:.+:(\d+)>$/, "$2")}.${emoji.split(" ")[0].replace(/^<(a)?:.+:(\d+)>$/, "$1") === "a" ? "gif" : "png"}`;
-    } else if (emoji.match(emojiRegex())) {
-      const codePoints = [];
-      for (const codePoint of emoji) {
-        codePoints.push(codePoint.codePointAt(0).toString(16));
-      }
-      return `https://cdn.jsdelivr.net/gh/jdecked/twemoji@latest/assets/72x72/${codePoints.join("-")}.png`;
-    } else {
-      this.success = false;
-      return "You need to provide a valid emoji to get an image!";
+    let emoji = this.options.emoji ?? this.content;
+    if (this.type === "classic" && this.message?.messageReference?.channelID && this.message.messageReference.messageID) {
+      const replyMessage = await this.client.rest.channels.getMessage(this.message.messageReference.channelID, this.message.messageReference.messageID).catch(() => undefined);
+      if (replyMessage) emoji = replyMessage.content;
     }
+    const matches = emoji.matchAll(/<(a?):[\w\d_]+:(\d+)>/g);
+    const urls = [];
+    for (const match of matches) {
+      urls.push(`https://cdn.discordapp.com/emojis/${match[2]}.${match[1] === "a" ? "gif" : "png"}`);
+    }
+    const emojiMatches = emoji.match(emojiRegex());
+    if (emojiMatches) {
+      for (const emoji of emojiMatches) {
+        const codePoints = [...emoji].map(v => v.codePointAt(0).toString(16)).join("-");
+        urls.push(`https://cdn.jsdelivr.net/gh/jdecked/twemoji@latest/assets/72x72/${codePoints}.png`);
+      }
+    }
+    if (urls.length > 0) return urls.join(" ");
+    this.success = false;
+    return "You need to provide a valid emoji to get an image!";
   }
 
   static flags = [{
