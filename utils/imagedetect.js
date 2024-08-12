b72f76534daa794c7edea895e5e1aa70828bb1b7
Essem <smswessem@gmail.com>
2024-08-12 09:41:19 -0500

    fix: Fix protocol check

--- utils/imagedetect.js
+++ utils/imagedetect.js
@@ -46,7 +46,7 @@ const getImage = async (image, image2, video, spoiler = false, extraReturnTypes 
   try {
     imageURL = new URL(image);
     if (!imageURL.host) throw null;
-    if (imageURL.protocol !== "http" && imageURL.protocol !== "https") throw null;
+    if (imageURL.protocol !== "http:" && imageURL.protocol !== "https:") throw null;
   } catch {
     return {
       url: image2,
