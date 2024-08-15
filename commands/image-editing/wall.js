85a8ecfe48e3e41f16b8d3cafd135b54a92d161d
Essem <smswessem@gmail.com>
2024-08-15 12:02:28 -0500

    revert: Revert "refactor: Remove tile and wall"
    This reverts commit c211c2b675e335a7ddec68fa01d386cf36afad89. These commands will be unavailable on the main bot for the time being.

---
+++ commands/image-editing/wall.js
@@ -0,0 +1,10 @@
+import ImageCommand from "../../classes/imageCommand.js";
+
+class WallCommand extends ImageCommand {
+  static description = "Creates a wall from an image";
+
+  static noImage = "You need to provide an image/GIF to make a wall!";
+  static command = "wall";
+}
+
+export default WallCommand;
---
+++ natives/wall.cc
@@ -0,0 +1,64 @@
+#ifdef MAGICK_ENABLED
+#include <Magick++.h>
+
+#include <cstring>
+#include <iostream>
+#include <list>
+
+#include "common.h"
+
+using namespace std;
+using namespace Magick;
+
+ArgumentMap Wall([[maybe_unused]] const string& type, string& outType, const char* bufferdata, size_t bufferLength, [[maybe_unused]] ArgumentMap arguments, size_t& dataSize)
+{
+  Blob blob;
+
+  list<Image> frames;
+  list<Image> coalesced;
+  list<Image> mid;
+  try {
+    readImages(&frames, Blob(bufferdata, bufferLength));
+  } catch (Magick::WarningCoder &warning) {
+    cerr << "Coder Warning: " << warning.what() << endl;
+  } catch (Magick::Warning &warning) {
+    cerr << "Warning: " << warning.what() << endl;
+  }
+  coalesceImages(&coalesced, frames.begin(), frames.end());
+
+  for (Image &image : coalesced) {
+    image.resize(Geometry("128x128"));
+    image.virtualPixelMethod(Magick::TileVirtualPixelMethod);
+    image.matteColor("none");
+    image.backgroundColor("none");
+    image.scale(Geometry("512x512"));
+    double arguments[16] = {0,   0, 57,  42, 0,   128, 63,  130,
+                            128, 0, 140, 60, 128, 128, 140, 140};
+    image.distort(Magick::PerspectiveDistortion, 16, arguments);
+    image.scale(Geometry("800x800>"));
+    image.magick(outType);
+    mid.push_back(image);
+  }
+
+  optimizeTransparency(mid.begin(), mid.end());
+
+  if (outType == "gif") {
+    for (Image &image : mid) {
+      image.quantizeDitherMethod(FloydSteinbergDitherMethod);
+      image.quantize();
+    }
+  }
+
+  writeImages(mid.begin(), mid.end(), &blob);
+
+  dataSize = blob.length();
+
+  char *data = reinterpret_cast<char*>(malloc(dataSize));
+  memcpy(data, blob.data(), dataSize);
+
+  ArgumentMap output;
+  output["buf"] = data;
+
+  return output;
+}
+#endif
