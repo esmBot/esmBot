4cd4f1f92f41f0476618385e08c1ef579de85c9d
Essem <smswessem@gmail.com>
2024-09-20 13:53:05 -0500

    refactor: Extract 8ball response strings

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -3,30 +3,30 @@ import { random } from "../../utils/misc.js";
 
 class EightBallCommand extends Command {
   static responses = [
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "Yes, definitely",
-    "You may rely on it",
-    "As I see it, yes",
-    "Most likely",
-    "Outlook good",
-    "Yes",
-    "Signs point to yes",
-    "Reply hazy, try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",
-    "Don't count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful"
+    "certain",
+    "decidedly",
+    "withoutDoubt",
+    "definitely",
+    "rely",
+    "seeIt",
+    "likely",
+    "outlookGood",
+    "yes",
+    "signs",
+    "hazy",
+    "later",
+    "betterNot",
+    "cannotPredict",
+    "concentrate",
+    "dontCount",
+    "replyNo",
+    "sourcesNo",
+    "outlookBad",
+    "doubtful"
   ];
 
   async run() {
-    return `🎱 ${random(EightBallCommand.responses)}`;
+    return `🎱 ${this.getString(`commands.responses.8ball.${random(EightBallCommand.responses)}`)}`;
   }
 
   static flags = [{
