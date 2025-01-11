6b652b8d148b169498ab3aa8eaa6720b94e7ef77
Essem <smswessem@gmail.com>
2025-01-11 13:36:06 -0600

    style: Format freeze.cc

--- natives/freeze.cc
+++ natives/freeze.cc
@@ -16,71 +16,71 @@ ArgumentMap Freeze(const string& type, string& outType, const char* bufferdata, 
   ArgumentMap output;
 
   if (type == "gif") {
-  char *fileData = reinterpret_cast<char*>(malloc(bufferLength));
-  memcpy(fileData, bufferdata, bufferLength);
+    char *fileData = reinterpret_cast<char*>(malloc(bufferLength));
+    memcpy(fileData, bufferdata, bufferLength);
 
-  char *match = const_cast<char*>("\x21\xFF\x0BNETSCAPE2.0\x03\x01");
-  char *descriptor = const_cast<char*>("\x2C\x00\x00\x00\x00");
-  char *lastPos;
+    char *match = const_cast<char*>("\x21\xFF\x0BNETSCAPE2.0\x03\x01");
+    char *descriptor = const_cast<char*>("\x2C\x00\x00\x00\x00");
+    char *lastPos;
 
-  bool none = true;
+    bool none = true;
 
-  if (loop) {
-    char *newData = reinterpret_cast<char*>(malloc(bufferLength + 19));
-    memcpy(newData, fileData, bufferLength);
-    lastPos = reinterpret_cast<char*>(memchr(newData, '\x2C', bufferLength));
-    while (lastPos != NULL) {
-      if (memcmp(lastPos, descriptor, 5) != 0) {
-        lastPos = reinterpret_cast<char*>(memchr(lastPos + 1, '\x2C',
-                                 (bufferLength - (lastPos - newData)) - 1));
-        continue;
+    if (loop) {
+      char *newData = reinterpret_cast<char*>(malloc(bufferLength + 19));
+      memcpy(newData, fileData, bufferLength);
+      lastPos = reinterpret_cast<char*>(memchr(newData, '\x2C', bufferLength));
+      while (lastPos != NULL) {
+        if (memcmp(lastPos, descriptor, 5) != 0) {
+          lastPos = reinterpret_cast<char*>(memchr(lastPos + 1, '\x2C',
+                                  (bufferLength - (lastPos - newData)) - 1));
+          continue;
+        }
+
+        memcpy(lastPos + 19, lastPos, (bufferLength - (lastPos - newData)));
+        memcpy(lastPos, match, 16);
+        memcpy(lastPos + 16, "\x00\x00\x00", 3);
+
+        none = false;
+        dataSize = bufferLength + 19;
+        break;
       }
+      if (none) dataSize = bufferLength;
 
-      memcpy(lastPos + 19, lastPos, (bufferLength - (lastPos - newData)));
-      memcpy(lastPos, match, 16);
-      memcpy(lastPos + 16, "\x00\x00\x00", 3);
+      output["buf"] = newData;
+    } else if (frame >= 0 && !loop) {
+      VImage in =
+          VImage::new_from_buffer(bufferdata, bufferLength, "",
+                                  GetInputOptions(type, true, false))
+              .colourspace(VIPS_INTERPRETATION_sRGB);
+      if (!in.has_alpha()) in = in.bandjoin(255);
 
-      none = false;
-      dataSize = bufferLength + 19;
-      break;
-    }
-    if (none) dataSize = bufferLength;
+      int pageHeight = vips_image_get_page_height(in.get_image());
+      int nPages = vips_image_get_n_pages(in.get_image());
+      int framePos = clamp(frame, 1, (int)nPages);
+      VImage out = in.crop(0, 0, in.width(), pageHeight * framePos);
+      out.set(VIPS_META_PAGE_HEIGHT, pageHeight);
+      out.set("loop", 1);
 
-    output["buf"] = newData;
-  } else if (frame >= 0 && !loop) {
-    VImage in =
-        VImage::new_from_buffer(bufferdata, bufferLength, "",
-                                GetInputOptions(type, true, false))
-            .colourspace(VIPS_INTERPRETATION_sRGB);
-    if (!in.has_alpha()) in = in.bandjoin(255);
+      char *buf;
+      out.write_to_buffer(("." + outType).c_str(), reinterpret_cast<void**>(&buf), &dataSize);
 
-    int pageHeight = vips_image_get_page_height(in.get_image());
-    int nPages = vips_image_get_n_pages(in.get_image());
-    int framePos = clamp(frame, 1, (int)nPages);
-    VImage out = in.crop(0, 0, in.width(), pageHeight * framePos);
-    out.set(VIPS_META_PAGE_HEIGHT, pageHeight);
-    out.set("loop", 1);
-
-    char *buf;
-    out.write_to_buffer(("." + outType).c_str(), reinterpret_cast<void**>(&buf), &dataSize);
-
-    output["buf"] = buf;
-  } else {
-    lastPos = reinterpret_cast<char*>(memchr(fileData, '\x21', bufferLength));
-    while (lastPos != NULL) {
-      if (memcmp(lastPos, match, 16) != 0) {
-        lastPos = reinterpret_cast<char*>(memchr(lastPos + 1, '\x21',
-                                 (bufferLength - (lastPos - fileData)) - 1));
-        continue;
+      output["buf"] = buf;
+    } else {
+      lastPos = reinterpret_cast<char*>(memchr(fileData, '\x21', bufferLength));
+      while (lastPos != NULL) {
+        if (memcmp(lastPos, match, 16) != 0) {
+          lastPos = reinterpret_cast<char*>(memchr(lastPos + 1, '\x21',
+                                  (bufferLength - (lastPos - fileData)) - 1));
+          continue;
+        }
+        memcpy(lastPos, lastPos + 19, (bufferLength - (lastPos - fileData)) - 19);
+        dataSize = bufferLength - 19;
+        none = false;
+        break;
       }
-      memcpy(lastPos, lastPos + 19, (bufferLength - (lastPos - fileData)) - 19);
-      dataSize = bufferLength - 19;
-      none = false;
-      break;
-    }
-    if (none) dataSize = bufferLength;
+      if (none) dataSize = bufferLength;
 
-    output["buf"] = fileData;
+      output["buf"] = fileData;
     }
   } else if (type == "webp") {
     WebPData webp_data;
@@ -98,7 +98,7 @@ ArgumentMap Freeze(const string& type, string& outType, const char* bufferdata, 
       if (frame >= 0 && !loop) {
         while (err > 0) {
           err = WebPMuxDeleteFrame(mux, frame + 1);
-  }
+        }
       }
       WebPMuxSetAnimationParams(mux, &anim);
     }
