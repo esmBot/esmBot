b4b88c4aaec85036d159ca9d1aa17c46f66121a8
Essem <smswessem@gmail.com>
2025-05-14 20:40:09 -0500

    fix: Fix ecosystem path

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "esmBot-manager",
-      script: "dist/utils/pm2/ext.js",
+      script: "dist/pm2/ext.js",
       autorestart: true,
       exp_backoff_restart_delay: 1000,
       watch: false,
