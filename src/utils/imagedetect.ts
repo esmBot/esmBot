3fd4e30f75b802807922d9f01809a55e18a7bb0f
Essem <smswessem@gmail.com>
2025-05-17 13:06:34 -0500

    fix: Ignore imagedetect permission checks when using interactions in DMs
    Bots have full access to their own one-on-one DM history with a user and implicitly have content access as well this way. The given permissions, however, do not reflect this, so a manual override is needed.
    
    Found this while looking at #505.

--- src/utils/imagedetect.ts
+++ src/utils/imagedetect.ts
@@ -273,7 +273,7 @@ export async function stickerDetect(
       !(channel instanceof PrivateChannel)
     )
       return;
-    if (perms && !perms.has("READ_MESSAGE_HISTORY")) return;
+    if (interaction?.authorizingIntegrationOwners?.[0] !== "0" && perms && !perms.has("READ_MESSAGE_HISTORY")) return;
     const messages = await channel.getMessages();
     // iterate over each message
     for (const message of messages) {
@@ -342,7 +342,7 @@ export default async (
       !(channel instanceof PrivateChannel)
     )
       return;
-    if (perms && !perms.has("READ_MESSAGE_HISTORY")) return;
+    if (interaction?.authorizingIntegrationOwners?.[0] !== "0" && perms && !perms.has("READ_MESSAGE_HISTORY")) return;
     const messages = await channel.getMessages();
     // iterate over each message
     for (const message of messages) {
