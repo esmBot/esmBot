4e196379292286b8088e3ffe498445dcbcc7b12f
Essem <smswessem@gmail.com>
2025-07-10 20:35:34 -0500

    feat: Add hue command

---
+++ commands/image-editing/hue.js
@@ -0,0 +1,41 @@
+import ImageCommand from "#cmd-classes/imageCommand.js";
+import { Constants } from "oceanic.js";
+
+class HueCommand extends ImageCommand {
+  paramsFunc() {
+    const shift = this.getOptionInteger("shift", true) ?? Number.parseInt(this.args[0]);
+    return {
+      color: "hueshift",
+      shift: Math.max(-180, Math.min(shift, 180)),
+    };
+  }
+
+  static init() {
+    super.init();
+    // required params need to be at the beginning of the array,
+    // so we use unshift instead of push here
+    this.flags.unshift({
+      name: "shift",
+      type: Constants.ApplicationCommandOptionTypes.INTEGER,
+      description: "Set the amount to shift by",
+      minValue: -180,
+      maxValue: 180,
+      classic: true,
+      required: true,
+    });
+    return this;
+  }
+
+  static description = "Hue shifts an image";
+
+  static requiresParam = true;
+  static requiredParam = "shift";
+  static requiredParamType = Constants.ApplicationCommandOptionTypes.INTEGER;
+  static noParam = "You need to provide the amount you want to hue shift by!";
+
+  static noImage = "You need to provide an image/GIF to hue shift!";
+  static command = "colors";
+  static aliases = ["hueshift", "recolor"];
+}
+
+export default HueCommand;
--- natives/colors.cc
+++ natives/colors.cc
@@ -12,6 +12,7 @@ VImage sepia = VImage::new_matrixv(3, 3, 0.3588, 0.7044, 0.1368, 0.2990, 0.5870,
 ArgumentMap Colors(const string &type, string &outType, const char *bufferdata, size_t bufferLength,
                    ArgumentMap arguments, bool *shouldKill) {
   string color = GetArgument<string>(arguments, "color");
+  int shift = GetArgumentWithFallback<int>(arguments, "shift", 0);
 
   VImage in = VImage::new_from_buffer(bufferdata, bufferLength, "", GetInputOptions(type, true, false))
                 .colourspace(VIPS_INTERPRETATION_sRGB);
@@ -22,6 +23,10 @@ ArgumentMap Colors(const string &type, string &outType, const char *bufferdata, 
     out = in.colourspace(VIPS_INTERPRETATION_B_W);
   } else if (color == "sepia") {
     out = in.extract_band(0, VImage::option()->set("n", 3)).recomb(sepia);
+  } else if (color == "hueshift") {
+    if (!in.has_alpha()) in = in.bandjoin(255);
+    vector<double> shiftVec = {0, 0, static_cast<double>(shift), 0};
+    out = in.colourspace(VIPS_INTERPRETATION_LCH) + shiftVec;
   }
 
   SetupTimeoutCallback(out, shouldKill);
