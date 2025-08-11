2a36f7f8ccebbf79d23c52262b574f33df828e15
Essem <smswessem@gmail.com>
2025-08-11 18:33:19 -0500

    refactor: Convert restart into async function
    This still doesn't completely work as intended, but at least it doesn't rely on a hackier syntax.

--- commands/general/restart.js
+++ commands/general/restart.js
@@ -1,19 +1,19 @@
 import process from "node:process";
 import Command from "#cmd-classes/command.js";
+import { exit } from "#utils/misc.js";
 
 class RestartCommand extends Command {
-  // eh, screw it
-  run() {
-    return new Promise((resolve) => {
-      const owners = process.env.OWNER?.split(",") ?? [];
-      if (!owners.includes(this.author.id)) {
-        this.success = false;
-        resolve(this.getString("commands.responses.restart.owner"));
-        return;
-      }
-      resolve(this.getString("commands.responses.restart.restarting"));
-      process.exit(1);
-    });
+  async run() {
+    const owners = process.env.OWNER?.split(",") ?? [];
+    if (!owners.includes(this.author.id)) {
+      this.success = false;
+      return this.getString("commands.responses.restart.owner");
+    }
+    try {
+      return this.getString("commands.responses.restart.restarting");
+    } finally {
+      await exit(this.client, this.database);
+    }
   }
 
   static description = "Restarts me";
