3ba67157582d814ec66e6c4be20fed60aba86d6e
Essem <smswessem@gmail.com>
2025-11-20 14:54:22 -0600

    style: A small bit of cleanup

--- natives/wall.cc
+++ natives/wall.cc
@@ -6,7 +6,7 @@ using namespace std;
 using namespace vips;
 
 ArgumentMap Wall(const string &type, string &outType, const char *bufferdata, size_t bufferLength,
-                  [[maybe_unused]] ArgumentMap arguments, bool *shouldKill) {
+                 [[maybe_unused]] ArgumentMap arguments, bool *shouldKill) {
   VImage in = VImage::new_from_buffer(bufferdata, bufferLength, "", GetInputOptions(type, false, false));
   if (!in.has_alpha()) in = in.bandjoin(255);
 
@@ -25,7 +25,7 @@ ArgumentMap Wall(const string &type, string &outType, const char *bufferdata, si
     }
   }
 
-  // We fit one tile inside a 128x128 box in order to 
+  // We fit one tile inside a 128x128 box in order to
   // reduce resolution, then blow it up to 512x512
   double maxSize = std::max(width, pageHeight);
   VImage tile = in.resize(128.0 / maxSize);
@@ -37,11 +37,8 @@ ArgumentMap Wall(const string &type, string &outType, const char *bufferdata, si
   // This distortion matrix was computed with `getPerspectiveTransform`
   // from OpenCV, with the original ImageMagick set of points:
   // {0, 0, 57, 42, 0, 128, 63, 130, 128, 0, 140, 60, 128, 128, 140, 140}
-  double T[] = {
-    1.32996610e+00, -9.06795066e-02, -7.19995282e+01,
-    -2.75152214e-01,  1.26875743e+00, -3.76041359e+01,
-    -7.70327830e-04, -7.08433645e-04
-  };
+  double T[] = {1.32996610e+00, -9.06795066e-02, -7.19995282e+01, -2.75152214e-01,
+                1.26875743e+00, -3.76041359e+01, -7.70327830e-04, -7.08433645e-04};
   VImage i = VImage::xyz(512, 512);
 
   VImage xi = (i[0] * T[0] + i[1] * T[1] + T[2]) / (i[0] * T[6] + i[1] * T[7] + 1);
@@ -54,8 +51,8 @@ ArgumentMap Wall(const string &type, string &outType, const char *bufferdata, si
   for (int i = 0; i < nPages; i++) {
     VImage img_frame = nPages > 1 ? tile.crop(0, i * pageHeight, width, pageHeight) : tile;
     // The 'extend' here doesn't do the tiling, it just fixes
-    // a weird border that forms around each tile. 
-    VImage frame = img_frame.mapim(index, VImage::option()->set("extend", VIPS_EXTEND_REPEAT)); 
+    // a weird border that forms around each tile.
+    VImage frame = img_frame.mapim(index, VImage::option()->set("extend", VIPS_EXTEND_REPEAT));
     img.push_back(frame);
   }
   VImage final = VImage::arrayjoin(img, VImage::option()->set("across", 1));
