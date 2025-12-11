7bd7c23a376b6625038d4da81aeb2714182f08fb
Essem <smswessem@gmail.com>
2025-12-10 21:29:36 -0600

    feat: Add snow flag to globe
    Happy holidays!

--- commands/image-editing/globe.js
+++ commands/image-editing/globe.js
@@ -1,6 +1,24 @@
+import { Constants } from "oceanic.js";
 import MediaCommand from "#cmd-classes/mediaCommand.js";
 
 class GlobeCommand extends MediaCommand {
+  paramsFunc() {
+    const snow = this.getOptionBoolean("snow");
+    return {
+      snow: !!snow,
+    };
+  }
+
+  static init() {
+    super.init();
+    this.flags.push({
+      name: "snow",
+      description: "Turns the globe into a snow globe",
+      type: Constants.ApplicationCommandOptionTypes.BOOLEAN,
+    });
+    return this;
+  }
+
   static description = "Spins an image";
   static aliases = ["sphere"];
 
