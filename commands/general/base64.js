093cff3e652b73797ae774be403cebb428ae183a
pach <84549814+pachxl@users.noreply.github.com>
2026-04-28 12:50:05 +0100

    remove a bunch of bullshit

--- commands/general/base64.js
+++
@@ -1,8 +0,0 @@
-import Command from "#cmd-classes/command.js";
-
-class Base64Command extends Command {
-  static description = "Encodes/decodes a Base64 string";
-  static aliases = ["b64"];
-}
-
-export default Base64Command;
