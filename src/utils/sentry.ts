28e681d02de5b6b1dc415daf0b21ca10acb0bfd6
Essem <smswessem@gmail.com>
2026-05-24 16:54:30 -0500

    style: Format

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -2,7 +2,9 @@ import process from "node:process";
 import detectRuntime from "./detectRuntime.ts";
 
 const { type } = detectRuntime();
-const Sentry: typeof import("@sentry/node-core") = await import(`@sentry/${type === "node" ? "node-core/light" : type}`);
+const Sentry: typeof import("@sentry/node-core") = await import(
+  `@sentry/${type === "node" ? "node-core/light" : type}`
+);
 
 Sentry.init({
   environment: process.env.NODE_ENV,
