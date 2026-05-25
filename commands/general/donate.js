32092343b31c7fc3430bf8777cb490cb57e7dc8f
Essem <smswessem@gmail.com>
2026-05-25 15:34:05 -0500

    feat: Use embed and new endpoint for donate command

--- commands/general/donate.js
+++ commands/general/donate.js
@@ -4,25 +4,45 @@ import logger from "#utils/logger.js";
 class DonateCommand extends Command {
   async run() {
     await this.acknowledge();
-    let prefix = "";
+    let desc = "";
     const controller = new AbortController();
     const timeout = setTimeout(() => {
       controller.abort();
     }, 5000);
     try {
-      const patrons = await fetch("https://projectlounge.pw/patrons", { signal: controller.signal }).then((data) =>
+      const patrons = await fetch("https://esmbot.net/patrons", { signal: controller.signal }).then((data) =>
         data.json(),
       );
       clearTimeout(timeout);
-      prefix = this.getString("commands.responses.donate.thanks");
       for (const patron of patrons) {
-        prefix += `\n**- ${patron}**`;
+        desc += `\n- **${patron}**`;
       }
-      prefix += "\n";
     } catch (e) {
       logger.error(`Unable to get patron data: ${e}`);
     }
-    return `${prefix}\n${this.getString("commands.responses.donate.support")} https://patreon.com/TheEssem`;
+    return {
+      embeds: [
+        {
+          author: {
+            name: this.getString("commands.responses.donate.thanks"),
+            iconURL: this.client.user.avatarURL(),
+          },
+          color: 0xff0000,
+          description: desc,
+          fields: [
+            {
+              name: this.getString("commands.responses.donate.like"),
+              value: this.getString("commands.responses.donate.support", {
+                params: {
+                  patreon: "https://patreon.com/TheEssem",
+                  kofi: "https://ko-fi.com/TheEssem",
+                },
+              }),
+            },
+          ],
+        },
+      ],
+    };
   }
 
   static description = "Learn more about how you can support esmBot's development";
