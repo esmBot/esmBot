893a4acda57049da22a26753fa4bddb28a6fa7bf
Samuel Hernandez <sam@samhza.com>
2026-09-19 22:55:11 -0400

    i18n: fix tag delete/owner using the prompt from tag view

--- commands/tags/tags/owner.js
+++ commands/tags/tags/owner.js
@@ -6,7 +6,7 @@ class TagsOwnerCommand extends Command {
     if (!this.guild) return this.getString("guildOnly");
     if (!this.permissions.has("EMBED_LINKS")) return this.getString("permissions.noEmbedLinks");
     const tagName = this.args[0] ?? this.getOptionString("name");
-    if (!tagName) return this.getString("commands.responses.tags.noInput");
+    if (!tagName) return this.getString("commands.responses.tags.ownerName");
     const getResult = await this.database.getTag(this.guild.id, tagName);
     if (!getResult) return this.getString("commands.responses.tags.invalid");
     const user = this.client.users.get(getResult.author);
